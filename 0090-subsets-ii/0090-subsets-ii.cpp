cbf614830ea9cc1e39b3bfffd3acad0464b59b8b
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-06-09 22:54:21 +0530

    Time: 3 ms (71.62%), Space: 7.6 MB (64.69%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    set<vector<int>> s;
+    vector<vector<int>> res;
     vector<int> temp;
     
     void subsequences(vector<int>& v, int index) {
-        int n = v.size();
-        if (index >= n) {
-            s.insert(temp);
-            return;
-        }
+       int n = v.size();
+       res.push_back(temp);
+       for(int i=index ; i<n;i++)
+       {
+           if(i>index && v[i]==v[i-1])
+           continue;
 
-        temp.push_back(v[index]);
-        subsequences(v, index + 1);
-        temp.pop_back();
-        subsequences(v, index + 1);
-    }
+           temp.push_back(v[i]);
+           subsequences(v,i+1);
+           temp.pop_back();
+       }
+}
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end()); // Sort the input array
         subsequences(nums, 0);
-        
-        vector<vector<int>> result(s.begin(), s.end());
-        return result;
-    }
+        return res;
+     }
 };
