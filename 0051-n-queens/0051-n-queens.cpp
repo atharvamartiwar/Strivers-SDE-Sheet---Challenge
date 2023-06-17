d9f2237a4c99919a3750603a6979ad4de84175fe
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-06-17 16:17:24 +0530

    Time: 10 ms (46.93%), Space: 7.1 MB (90.95%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isSafe(int x,int y, vector<string> &grid)
+    {
+        int  n = (int)grid.size();
+        for(int i=0;i<n;i++)
+        {
+            if(grid[i][y]=='Q') return false;  //check for same columns
+         
+         for(int j=0;j<n;j++)
+        {
+            if(i-j == x-y && grid[i][j]=='Q') return false;
+            if(i+j==x+y && grid[i][j]=='Q')  return false;
+        }
+        }
+     return true;
+
+    }
+
+
+    bool Nqueen(int row, vector<string>&grid,vector<vector<string>>&res)
+    {
+        int n = (int)grid.size();
+       
+        if(row>=n)
+        {
+            res.push_back(grid);
+            return true;
+        }
+
+        for(int col=0;col<n;col++)
+        {
+            if(isSafe(row,col,grid))
+            {
+                grid[row][col]='Q';
+
+                Nqueen(row+1,grid,res);
+                grid[row][col]='.';
+            }
+        }
+
+        return false;
+    }
+
+
+    vector<vector<string>> solveNQueens(int n) {
+      vector<vector<string>> res;
+      vector<string> grid(n, string(n, '.'));
+      Nqueen(0,grid,res);
+      return res;
+ }
+};
