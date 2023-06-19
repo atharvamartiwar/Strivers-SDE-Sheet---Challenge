aa61f885753d6c06f82516f52cabff553a9524ea
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-06-19 15:16:06 +0530

    Time: 433 ms (82.46%), Space: 127.9 MB (80.26%) - LeetHub

---
+++ 2240-intervals-between-identical-elements/2240-intervals-between-identical-elements.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<long long> getDistances(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> res(n);
+        unordered_map<int, vector<int>> mp;
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            mp[num].push_back(i);
+        }
+        for ( auto x : mp) {
+            int num = x.first;
+            vector<int>&indices  = x.second;
+            long long sumBefore = 0;
+            long long totalSum = 0;
+          
+             for(auto x:indices)
+             {
+                 totalSum+=(long long)x;
+             }
+
+            int n = (int)indices.size();
+            for (int i = 0; i < n; i++) {
+                int index = indices[i];
+                long long sumAfter = totalSum-sumBefore-index;
+                res[index] += (index*(long long)i);
+                res[index] -= (index*(long long)(n-i-1)) ;
+                res[index] -= sumBefore;
+                res[index] += sumAfter;
+
+                sumBefore+=index;
+               
+            }
+        }
+
+        return res;
+    }
+};
