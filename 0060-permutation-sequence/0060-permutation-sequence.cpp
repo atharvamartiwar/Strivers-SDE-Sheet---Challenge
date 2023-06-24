407d3e5d916f3228d60c2cda71ebece2a11171a8
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-06-24 17:30:40 +0530

    Time: 4 ms (55.62%), Space: 6 MB (47.95%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,24 +1,56 @@
+// class Solution {
+// public:
+//     string getPermutation(int n, int k) {
+//         vector<int> v(n);
+//         for(int i=0;i<n;i++)
+//         {
+//            v[i]=i+1;
+//         }
+
+//          for(int i=0;i<k-1;i++)
+//         {
+//            next_permutation(v.begin(),v.end());
+//         }
+
+//         string str="";
+
+//         for(int i=0;i<n;i++)
+//         {
+//             str+=to_string(v[i]);
+//         }
+
+//         return str;
+//  }
+// };
 class Solution {
 public:
     string getPermutation(int n, int k) {
         vector<int> v(n);
-        for(int i=0;i<n;i++)
+        int fact = 1;
+        for(int i=1;i<=n;i++)
         {
-           v[i]=i+1;
+           v[i-1]=i;
+           fact=fact*(i);
         }
 
-         for(int i=0;i<k-1;i++)
+        fact/=n;
+
+        string str = "";
+        k = k-1;
+        while(1)
         {
-           next_permutation(v.begin(),v.end());
+            str = str + to_string(v[k/fact]);
+            auto it = v.begin();
+            v.erase(it + k/fact);
+            if(v.size()==0)
+            {
+                break;
+            }
+            k = k%fact;
+            fact/=v.size();
         }
 
-        string str="";
-
-        for(int i=0;i<n;i++)
-        {
-            str+=to_string(v[i]);
-        }
-
-        return str;
+     return str;
+        
  }
 };
