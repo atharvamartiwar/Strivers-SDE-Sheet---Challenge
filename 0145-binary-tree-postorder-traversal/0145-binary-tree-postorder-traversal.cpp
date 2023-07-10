f424d4e975b64a3d11b60cd65fb9ccee899a0030
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-07-11 02:08:55 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.53%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,19 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> ans;
+//     void postorder(TreeNode* root)
+//     {
+//         if(root==NULL) return;
+//         postorder(root->left);
+//         postorder(root->right);
+//         ans.push_back(root->val);
+
+//     }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         postorder(root);
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    vector<int> ans;
-    void postorder(TreeNode* root)
-    {
-        if(root==NULL) return;
-        postorder(root->left);
-        postorder(root->right);
-        ans.push_back(root->val);
+ vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*>st1,st2;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL) st1.push(root->left);
+            if(root->right!=NULL) st1.push(root->right);
+            
+        }
 
-    }
-    vector<int> postorderTraversal(TreeNode* root) {
-        postorder(root);
+        while(!st2.empty())
+        {
+            TreeNode* r = st2.top();
+            ans.push_back(r->val);
+            st2.pop();
+        }
+
         return ans;
     }
 };
