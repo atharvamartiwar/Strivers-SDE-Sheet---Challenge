761cfda1257bed9afa7c205c8279682a83813403
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-07-11 00:59:51 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.88%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,18 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+// vector<int> ans;
+//    void inorder(TreeNode*root)
+//    {
+//        if(root==NULL) return;
+//        inorder(root->left);
+//        ans.push_back(root->val);
+//        inorder(root->right);
+//    }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         inorder(root);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-vector<int> ans;
-   void inorder(TreeNode*root)
-   {
-       if(root==NULL) return;
-       inorder(root->left);
-       ans.push_back(root->val);
-       inorder(root->right);
-   }
-    vector<int> inorderTraversal(TreeNode* root) {
-        inorder(root);
-        return ans;
+vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        TreeNode* node = root;
+        vector<int> ans;
+        while(true)
+        {
+            if(node!=NULL)
+            {
+                st.push(node);   
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty()) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+       return ans;
     }
 };
