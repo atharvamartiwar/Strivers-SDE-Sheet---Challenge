728bd0bf51232893e4a18578022e9248021c2ed3
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-09-10 13:18:28 +0530

    Time: 3 ms (29.32%), Space: 6.6 MB (8.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,40 @@
+// class Solution {
+// public:
+//     //vector<vector<int>> dp(m,vector<int>dp(n,-1));
+//     int rec(int row, int col,vector<vector<int>>&dp)
+//     {
+//         if(row==0 && col == 0) return dp[row][col]=1;
+//         if(row<0 || col<0) return 0;
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         int up = rec(row-1,col,dp);
+//         int left = rec(row,col-1,dp);
+
+//         return dp[row][col] = up+left;
+        
+//     }
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         return rec(m-1,n-1,dp);
+//      }
+// };
 class Solution {
 public:
-    //vector<vector<int>> dp(m,vector<int>dp(n,-1));
-    int rec(int row, int col,vector<vector<int>>&dp)
-    {
-        if(row==0 && col == 0) return dp[row][col]=1;
-        if(row<0 || col<0) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int up = rec(row-1,col,dp);
-        int left = rec(row,col-1,dp);
-
-        return dp[row][col] = up+left;
+   int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                int up=0,left=0;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = up+left;
+                }
+            }
+        }
         
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return rec(m-1,n-1,dp);
-       
-
-    }
+        return dp[m-1][n-1];
+     }
 };
