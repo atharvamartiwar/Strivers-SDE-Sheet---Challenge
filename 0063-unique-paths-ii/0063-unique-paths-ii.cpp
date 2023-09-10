3b779c098c725519c8f8b5b84a9b4ab3c12406e4
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-09-10 17:12:34 +0530

    Time: 100 ms (5.47%), Space: 72.3 MB (9.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,12 +3,11 @@ public:
    
 int rec(int i,int j,vector<vector<int>>mat,vector<vector<int>>&dp)
     {
-        
-        if(i>=0 && j>=0 && mat[i][j]==1 ) return 0;
-        if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        cout<<i<<" "<<j<<":  "<<dp[i][j]<<endl;
+        if(i>=0 && j>=0 && mat[i][j]==1 ) return dp[i][j]=0;
+        if(i==0 && j==0) return 1;
+        // cout<<i<<" "<<j<<":  "<<dp[i][j]<<endl;
          int up = rec(i-1,j,mat,dp);
          int left = rec(i,j-1,mat,dp);
         return dp[i][j]= (up+left);
