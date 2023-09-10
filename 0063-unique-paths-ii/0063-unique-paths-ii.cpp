263fe459c76f70973d2e676dd31c4ee98988b1d8
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-09-10 18:06:27 +0530

    Time: 0 ms (100%), Space: 8 MB (9.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -23,26 +23,3 @@ int rec(int i,int j)
 
     }
 };
-// class Solution {
-// public:
-//    vector<vector<int>>mat;
-//    vector<vector<int>>dp;
-// int rec(int i,int j)
-//     {
-//         if(i<0 || j<0) return 0;
-//         if(dp[i][j]!=-1) return dp[i][j];
-//         if(i>=0 && j>=0 && mat[i][j]==1 ) return dp[i][j]=0;
-//         if(i==0 && j==0) return 1;
-//          int up = rec(i-1,j);
-//          int left = rec(i,j-1);
-//         return dp[i][j]= (up+left);
-//     }
-//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-//         mat = obstacleGrid;
-//         int m = (int)obstacleGrid.size();
-//         int n = (int)obstacleGrid[0].size();
-//         dp.resize(m,vector<int>(n,-1));
-//         return rec(m-1,n-1);
-
-//     }
-// };
