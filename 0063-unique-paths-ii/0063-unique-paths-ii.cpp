1406164021147e0cbcd81e0c0e09dd3d5682a7b7
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-09-10 17:11:25 +0530

    Time: 88 ms (5.47%), Space: 72.4 MB (9.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,7 +4,7 @@ public:
 int rec(int i,int j,vector<vector<int>>mat,vector<vector<int>>&dp)
     {
         
-        if(i>=0 && j>=0 && mat[i][j]==1 ) return dp[i][j]=0;
+        if(i>=0 && j>=0 && mat[i][j]==1 ) return 0;
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
